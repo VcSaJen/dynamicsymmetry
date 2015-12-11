d49e07879626f597ccffe2677a2962d78d59ffbd
VcSaJen <VcSaJen@ya.ru>
2015-12-11 15:59:49 +0900

    Sponge Empty Project

---
+++ src/main/java/com/vcsajen/dynamicsymmetry/DynamicSymmetry.java
@@ -0,0 +1,8 @@
+package com.vcsajen.dynamicsymmetry;
+
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = "dynamicsymmetry", name = "DynamicSymmetry", version = "1.0")
+public class DynamicSymmetry {
+
+}
