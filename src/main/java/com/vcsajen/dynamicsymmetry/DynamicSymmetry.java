8178a6704c0e083f5d1350439886f89e764ac8be
VcSaJen <VcSaJen@ya.ru>
2015-12-11 21:02:22 +0900

    Simple Command helloworld test

--- src/main/java/com/vcsajen/dynamicsymmetry/DynamicSymmetry.java
+++ src/main/java/com/vcsajen/dynamicsymmetry/DynamicSymmetry.java
@@ -1,8 +1,39 @@
 package com.vcsajen.dynamicsymmetry;
 
+import org.spongepowered.api.Game;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.text.Texts;
+import org.spongepowered.api.command.spec.CommandSpec;
 
 @Plugin(id = "dynamicsymmetry", name = "DynamicSymmetry", version = "1.0")
-public class DynamicSymmetry {
+public class DynamicSymmetry
+{
+    private Game game;
 
+    public CommandResult executehw(CommandSource src, CommandContext args) throws CommandException
+    {
+        src.sendMessage(Texts.of("Hello World!"));
+        return CommandResult.success();
+    }
+
+    @Listener
+    public void onGameInitialization(GameInitializationEvent event)
+    {
+        game = Sponge.getGame();
+
+        CommandSpec myCommandSpec = CommandSpec.builder()
+                .description(Texts.of("Hello World Command"))
+                .permission("myplugin.command.helloworld")
+                .executor(this::executehw)
+                .build();
+
+        game.getCommandManager().register(this, myCommandSpec, "helloworld", "hello", "test");
+    }
 }
