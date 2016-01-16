35b2b031cee8a81ed47b72659e6249b435606ed5
VcSaJen <VcSaJen@ya.ru>
2016-01-16 23:38:23 +0900

    SpongeAPI 3.0.0 fix.

--- src/main/java/com/vcsajen/dynamicsymmetry/DynamicSymmetry.java
+++ src/main/java/com/vcsajen/dynamicsymmetry/DynamicSymmetry.java
@@ -9,7 +9,7 @@ import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.plugin.Plugin;
-import org.spongepowered.api.text.Texts;
+import org.spongepowered.api.text.Text;
 import org.spongepowered.api.command.spec.CommandSpec;
 
 @Plugin(id = "dynamicsymmetry", name = "DynamicSymmetry", version = "1.0")
@@ -19,7 +19,7 @@ public class DynamicSymmetry
 
     public CommandResult executehw(CommandSource src, CommandContext args) throws CommandException
     {
-        src.sendMessage(Texts.of("Hello World!"));
+        src.sendMessage(Text.of("Hello World!"));
         return CommandResult.success();
     }
 
@@ -29,7 +29,7 @@ public class DynamicSymmetry
         game = Sponge.getGame();
 
         CommandSpec myCommandSpec = CommandSpec.builder()
-                .description(Texts.of("Hello World Command"))
+                .description(Text.of("Hello World Command"))
                 .permission("myplugin.command.helloworld")
                 .executor(this::executehw)
                 .build();
